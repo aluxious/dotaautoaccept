4ff4c4897ed127ed4133638aa1ed1620fd56a6f4
aluxious <aluxious@gmail.com>
2022-01-31 07:37:12 +0800

    added party accept and continue(red) buttons refactored code grouped test images in a separate folder

--- dotaauto.py
+++ dotaauto.py
@@ -7,38 +7,27 @@ import os
 
 BUTTON_ACCEPT = "accept_button.png"
 BUTTON_READY = "ready_button.png"
+BUTTON_PARTY = "party_button.png"
 BUTTON_CONTINUE = "continue_button.png"
-if not (os.path.exists(".\\"+BUTTON_ACCEPT) and os.path.exists(".\\"+BUTTON_READY) and os.path.exists(".\\"+BUTTON_CONTINUE)):
+BUTTON_CONTINUE_RED = "continue_button_red.png"
+
+if not (os.path.exists(".\\"+BUTTON_ACCEPT) and os.path.exists(".\\"+BUTTON_READY) and os.path.exists(".\\"+BUTTON_CONTINUE) 
+and os.path.exists(".\\"+BUTTON_CONTINUE_RED) and os.path.exists(".\\"+BUTTON_PARTY)):
     print('Missing image file(s)!!!')
     exit()
 
-width_acc, height_acc = pyautogui.size()
-center_acc = pyautogui.center((0, 0, width_acc, height_acc))
-isfound_accept = None
-
-width_rdy, height_rdy = pyautogui.size()
-center_rdy = pyautogui.center((-180, 120, width_rdy, height_rdy))
-isfound_ready = None
-
-width_con, height_con = pyautogui.size()
-center_con = pyautogui.center((0, 505, width_rdy, height_rdy))
-isfound_continue = None
+def locate_and_click(button_file, button_name):
+    button_loc = pyautogui.locateOnScreen(button_file, grayscale=False, confidence=0.9)
+    if button_loc:
+        print ('Found ' + button_name + ' button')
+        pyautogui.click(button_loc)
+        sleep(5)
 
 print ('Waiting to accept')
 while True:
-    isfound_accept = pyautogui.locateOnScreen(BUTTON_ACCEPT, grayscale=False, confidence=0.9)
-    if isfound_accept:
-        print ('Found accept button')
-        pyautogui.click(center_acc)
-        sleep(5)
-    isfound_ready = pyautogui.locateOnScreen(BUTTON_READY, grayscale=False, confidence=0.9)
-    if isfound_ready:
-        print ('Found ready button')
-        pyautogui.click(center_rdy)
-        sleep(5)
-    isfound_continue = pyautogui.locateOnScreen(BUTTON_CONTINUE, grayscale=False, confidence=0.9)
-    if isfound_continue:
-        print ('Found continue button')
-        pyautogui.click(center_con)
-        sleep(5)      
+    locate_and_click(BUTTON_ACCEPT, "accept")
+    locate_and_click(BUTTON_READY, "ready")
+    locate_and_click(BUTTON_PARTY, "accept party")
+    locate_and_click(BUTTON_CONTINUE, "continue")
+    locate_and_click(BUTTON_CONTINUE_RED, "red continue")
     sleep(1)
